db9d6bbec248671bb7db0b1333d8562d70c68b0a
Maksym <chornyimax2@gmail.com>
2026-01-04 22:20:32 -0600

    feat: Add MPU-6050 sensor integration per docs

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 # These two modules allow us to run a web server.
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-# This module lets us pick random numbers.
 import random
-# Add this line to import the BMP180 library:
+# Import sensor libraries
 from bmp180 import BMP180
+from mpu6050 import mpu6050
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# --- SENSOR INITIALIZATION ---
+
 # Create an object to represent our BMP180 sensor
 bmp = BMP180()
+
+# Create an object for MPU-6050. 0x68 is the default I2C address.
+mpu = mpu6050(0x68)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# This function runs in the background...
 def background_thread():
     while True:
-        socketio.sleep(1)
+        socketio.sleep(1) # Send data every 1 second
 
-        # First, we ask our sensor object for the current pressure
+        # 1. Read Barometric Pressure
         barometricPressure = bmp.get_pressure()
 
-        # Now, we add it to the message we're sending
+        # 2. Read Accelerometer and Gyroscope Data
+        # Returns a dictionary with 'x', 'y', and 'z' keys
+        accel_data = mpu.get_accel_data()
+        gyro_data = mpu.get_gyro_data()
+
+        # Emit all data to the client
         socketio.emit(
             'update_data',
             {
                 'randomNumber': random.randint(1, 100),
-                # Add a new pair for our pressure data
-                'barometricPressure': barometricPressure
+                'barometricPressure': barometricPressure,
+                # Accelerometer data (m/s^2)
+                'accelX': round(accel_data['x'], 2),
+                'accelY': round(accel_data['y'], 2),
+                'accelZ': round(accel_data['z'], 2),
+                # Gyroscope data (degrees)
+                'gyroX': round(gyro_data['x'], 2),
+                'gyroY': round(gyro_data['y'], 2),
+                'gyroZ': round(gyro_data['z'], 2)
             }
         )
 
@@ -39,11 +55,9 @@ def handle_connect():
     print('Client connected')
     socketio.start_background_task(target=background_thread)
 
-# Added: Listen for the 'do_a_thing' event from the client
+# Interaction Event (from previous step)
 @socketio.on('do_a_thing')
 def do_a_thing(msg):
-    # 'msg' will contain the data sent from the client
-    # Let's print what we just received to the console!
     print(msg['hello'])
 
 def main():
