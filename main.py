6e7f0f29e512db24ea6580abe7fdba618b8f217a
Maksym <chornyimax2@gmail.com>
2026-01-04 22:33:00 -0600

    feat: Add motor control logic via GPIO

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ import time
 # Import sensor libraries provided by SDK
 from bmp180 import BMP180
 from mpu6050 import mpu6050
-# Add TF-Luna library import:
 from tfluna import TFLuna
+# Import GPIO library for motor control
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# --- SENSOR INITIALIZATION ---
+# --- MOTOR CONFIGURATION ---
+# Define our motor controller pins as per documentation
+IN1 = 12
+IN2 = 13
 
+# Set the GPIO numbering mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up the GPIO pins as output
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+
+# --- SENSOR INITIALIZATION ---
 print("INFO: Initializing hardware...")
 
 # 1. Barometer
@@ -23,10 +35,10 @@ bmp = BMP180()
 # 2. Accelerometer/Gyroscope
 mpu = mpu6050(0x68)
 
-# 3. LiDAR (TF-Luna) - Setup according to docs
-tfluna = TFLuna()        # Create object
-tfluna.open()            # Open connection
-tfluna.set_samp_rate(5)  # Set to 5Hz as recommended in docs
+# 3. LiDAR (TF-Luna)
+tfluna = TFLuna()
+tfluna.open()
+tfluna.set_samp_rate(5)
 
 @app.route('/')
 def index():
@@ -34,21 +46,13 @@ def index():
 
 def background_thread():
     while True:
-        socketio.sleep(1) 
-
+        socketio.sleep(1)
         try:
             # --- READ SENSORS ---
-            
-            # BMP180
             pressure = bmp.get_pressure()
-            
-            # MPU-6050
             accel_data = mpu.get_accel_data()
             gyro_data = mpu.get_gyro_data()
-            
-            # TF-Luna
-            # This uses special syntax called a "tuple" to set all three variables
-            distance, strength, temperature = tfluna.read()
+            distance, strength, temp = tfluna.read()
 
             # --- SEND DATA ---
             socketio.emit(
@@ -56,7 +60,6 @@ def background_thread():
                 {
                     'randomNumber': random.randint(1, 100),
                     'barometricPressure': pressure,
-                    
                     # MPU Data
                     'accelX': round(accel_data['x'], 2),
                     'accelY': round(accel_data['y'], 2),
@@ -64,8 +67,7 @@ def background_thread():
                     'gyroX': round(gyro_data['x'], 2),
                     'gyroY': round(gyro_data['y'], 2),
                     'gyroZ': round(gyro_data['z'], 2),
-                    
-                    # LiDAR Data (Convert to cm for display)
+                    # LiDAR Data
                     'lidarDistance': round(distance * 100.0, 2)
                 }
             )
@@ -77,13 +79,32 @@ def handle_connect():
     print('Client connected')
     socketio.start_background_task(target=background_thread)
 
-# Interaction Event
-@socketio.on('do_a_thing')
-def do_a_thing(msg):
-    print(msg['hello'])
+# --- MOTOR CONTROL LOGIC ---
+@socketio.on('control_motor')
+def handle_motor_control(message):
+    """Handles motor control events from the client."""
+    action = message.get('action')
+    print(f"EVENT: Received 'control_motor' with action: '{action}'")
+    
+    if action == 'forward':
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+    elif action == 'backward':
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+    elif action == 'stop':
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.LOW)
+    else:
+        print(f"WARNING: Unknown motor action '{action}'")
 
 def main():
     socketio.run(app, host='0.0.0.0', port=80, allow_unsafe_werkzeug=True)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        # Clean up GPIO on exit
+        GPIO.cleanup()
+        print("INFO: Server stopped and GPIO cleaned up.")
